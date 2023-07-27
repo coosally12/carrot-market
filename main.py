06e1287203cbcc117b8aed477a371c48fe4808d1
coosally12 <135309177+coosally12@users.noreply.github.com>
2023-07-27 14:45:24 +0900

    🐛🐛orjson두번째 버그 수정
    삭제할 부분 삭제하고 다시 수정

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI, UploadFile, Form, Response
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 from fastapi.staticfiles import StaticFiles
 from typing import Annotated
 import sqlite3
