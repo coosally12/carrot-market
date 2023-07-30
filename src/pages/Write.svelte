73f318ecca729396ce7b645e7ca9c428d6aca7bb
coosally12 <135309177+coosally12@users.noreply.github.com>
2023-07-30 17:55:34 +0900

    서버 중복 안되게 수정

--- src/pages/Write.svelte
+++ src/pages/Write.svelte
@@ -1,23 +1,48 @@
-<form id="write-form">
-  <div>
+<script>
+  import { getDatabase, ref, push } from "firebase/database";
+  import { push } from "svelte-spa-router";
+
+  let title;
+  let price;
+  let description;
+  let place;
+
+  function writeUserData() {
+    const db = getDatabase();
+    push(ref(db, "items/"), {
+      title,
+      price,
+      description,
+      place,
+    });
+  }
+</script>
+
+<form id="write-form" on:submit|preventDefault={writeUserData}>
+  <!-- <div>
     <label for="image">이미지</label>
     <input type="file" id="image" name="image" />
-  </div>
+  </div> -->
   <div>
     <label for="title">제목</label>
-    <input type="text" id="title" name="title" />
+    <input type="text" id="title" name="title" bind:value={title} />
   </div>
   <div>
     <label for="price">가격</label>
-    <input type="number" id="price" name="price" />
+    <input type="number" id="price" name="price" bind:value={price} />
   </div>
   <div>
     <label for="description">설명</label>
-    <input type="text" id="description" name="description" />
+    <input
+      type="text"
+      id="description"
+      name="description"
+      bind:value={description}
+    />
   </div>
   <div>
     <label for="place">장소</label>
-    <input type="text" id="place" name="place" />
+    <input type="text" id="place" name="place" bind:value={place} />
   </div>
   <div>
     <button type="submit">글쓰기 완료!</button>
