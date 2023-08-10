2a816b4d4b74c10a4eb5e03465965c4f8d5e81d6
coosally12 <135309177+coosally12@users.noreply.github.com>
2023-08-10 12:45:22 +0900

    글쓰기 창 데이터베이스 구현

--- firebase.js
+++ firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
 const firebaseConfig = {
-  // ...
-  // The value of `databaseURL` depends on the location of the database
   databaseURL:
     "https://carrot-market-e03a8-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  storageBucket: "gs://carrot-market-e03a8.appspot.com",
 };
 
 // Initialize Firebase
@@ -15,3 +15,4 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Realtime Database and get a reference to the service
 const database = getDatabase(app);
+const storage = getStorage(app);
